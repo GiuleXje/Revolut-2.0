7266a9bea3ba11eb83660b52ddf85fe1900297e8
Pal Roberto-Giulio <151451151+GiuleXje@users.noreply.github.com>
2024-12-30 11:13:53 +0200

    Update Main.java

--- src/main/java/org/poo/main/Main.java
+++ src/main/java/org/poo/main/Main.java
@@ -3,9 +3,22 @@ package org.poo.main;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
 import com.fasterxml.jackson.databind.node.ArrayNode;
+
+import org.poo.BankUsers.User;
+import org.poo.BankUsers.EmailDB;
+import org.poo.BankUsers.AccountDB;
+import org.poo.BankUsers.CardDB;
+import org.poo.BankUsers.IBANDB;
+import org.poo.BankUsers.AliasDB;
+import org.poo.BankingOperations.BankOpData;
+import org.poo.ExchangeRate.ExchangeRate;
 import org.poo.checker.Checker;
 import org.poo.checker.CheckerConstants;
+import org.poo.fileio.CommandInput;
+import org.poo.fileio.ExchangeInput;
 import org.poo.fileio.ObjectInput;
+import org.poo.fileio.UserInput;
+import org.poo.utils.Utils;
 
 import java.io.File;
 import java.io.IOException;
@@ -29,6 +42,7 @@ public final class Main {
     /**
      * DO NOT MODIFY MAIN METHOD
      * Call the checker
+     *
      * @param args from command line
      * @throws IOException in case of exceptions to reading / writing
      */
@@ -67,31 +81,54 @@ public final class Main {
      * @throws IOException in case of exceptions to reading / writing
      */
     public static void action(final String filePath1,
-                              final String filePath2) throws IOException {
+                              final String filePath2)
+            throws IOException {
         ObjectMapper objectMapper = new ObjectMapper();
         File file = new File(CheckerConstants.TESTS_PATH + filePath1);
         ObjectInput inputData = objectMapper.readValue(file, ObjectInput.class);
 
         ArrayNode output = objectMapper.createArrayNode();
 
-        /*
-         * TODO Implement your function here
-         *
-         * How to add output to the output array?
-         * There are multiple ways to do this, here is one example:
-         *
-         * ObjectMapper mapper = new ObjectMapper();
-         *
-         * ObjectNode objectNode = mapper.createObjectNode();
-         * objectNode.put("field_name", "field_value");
-         *
-         * ArrayNode arrayNode = mapper.createArrayNode();
-         * arrayNode.add(objectNode);
-         *
-         * output.add(arrayNode);
-         * output.add(objectNode);
-         *
-         */
+        // get all the data needed from the input
+        UserInput[] usersInfo = inputData.getUsers();
+        ExchangeInput[] exRates = inputData.getExchangeRates();
+        CommandInput[] commands = inputData.
+                getCommands();
+
+        // create a database containing all the users' data,
+        // accessible by their email address
+        EmailDB emailDB = new EmailDB();
+        // create a database that can return each IBAN's user
+        IBANDB ibanDB = new IBANDB();
+        // create a database that can return a card's bank account
+        CardDB cardDB = new CardDB();
+        // set a new exchange rate graph
+        ExchangeRate exchangeRate = new ExchangeRate(exRates);
+        // set a new alias database
+        AliasDB aliasDB = new AliasDB();
+        // set a new accounts(IBAN) database
+        AccountDB accountDB = new AccountDB();
+
+        //reset the card and IBAN generator
+        Utils.resetRandom();
+
+        // get the users
+        for (UserInput user : usersInfo) {
+            emailDB.addUser(new User(user.getFirstName(),
+                    user.getLastName(), user.getEmail()));
+        }
+
+        // get each action
+        for (CommandInput command : commands) {
+            BankOpData commandHandler = new BankOpData(command, emailDB, ibanDB, cardDB,
+                    exchangeRate, aliasDB, accountDB);
+            commandHandler.execute(); // handle the given command
+
+            // output for JSON, if needed
+            if (commandHandler.getReturnVal() != null) {
+                output.add(commandHandler.getReturnVal());
+            }
+        }
 
         ObjectWriter objectWriter = objectMapper.writerWithDefaultPrettyPrinter();
         objectWriter.writeValue(new File(filePath2), output);
@@ -104,9 +141,9 @@ public final class Main {
      * @return the extracted numbers
      */
     public static int fileConsumer(final File file) {
-        return Integer.parseInt(
-                file.getName()
-                        .replaceAll(CheckerConstants.DIGIT_REGEX, CheckerConstants.EMPTY_STR)
-        );
+        String fileName = file.getName()
+                .replaceAll(CheckerConstants.DIGIT_REGEX,
+                        CheckerConstants.EMPTY_STR);
+        return Integer.parseInt(fileName.substring(0, 2));
     }
 }
